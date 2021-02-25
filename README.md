044c076ee7f9f3675b3c5608ebac4f4c81da559a
Emerson Rocha <rocha@ieee.org>
2021-02-24 23:24:17 -0300

    Bootstraped pip package files; needs to undestand better how namespacing works

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # hxlm-crypto-eticaai
 [proof-of-concept[not-production-ready] @see https://github.com/EticaAI/HXL-Data-Science-file-formats
+
+
+<!--
+Internal temporary notes
+
+python3 -m pip install --upgrade build
+python3 -m build
+
+-->
---
+++ hxlm/crypto/meta.py
@@ -0,0 +1,9 @@
+HXLM_CRYPTO_META = {
+    # path: hxml.crypto
+    # 'code': "xz_eticaai",
+    # 'code_if_approved': "xz_eticaai",
+    # TODO: update title
+    'title': "Etica.AI is concerned about long-term social & ethical " +
+             "implications of artificial intelligence on Africa/Latin " +
+             " America. Do it via grassroots acts in short-term"
+}
